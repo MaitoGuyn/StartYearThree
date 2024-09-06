bab2609af99c7636a81fc1bf25fdc6d9e0e0f057
Сафиуллин Самат <samatsafiullin7@gmail.com>
2024-09-06 09:26:48 +0300

    1234

--- StartYearThree/MainWindow.xaml.cs
+++ StartYearThree/MainWindow.xaml.cs
@@ -29,8 +29,23 @@ namespace StartYearThree
             List<string> Names = pers.Select(p => p.FName).ToList();
             FName.ItemsSource = Names;
         }
-
-        
        
+        private void ConsiderBTN_Click(object sender, RoutedEventArgs e)
+        {
+            int input;
+            int output;
+            DateTime DatePerson;
+            if (int.TryParse(Money.Text,out input))
+            {
+                output = input * 2;
+                FullMoneyEnd_Copy.Text = output.ToString();
+            }
+            else
+            {
+                
+            }
+            //input = Convert.ToInt32(Money.Text);
+            //DatePerson = Convert.ToDateTime(Money.Text);
+        }
     }
 }
